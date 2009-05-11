24f6233c2d92f375e05bb6a5e5d1bea3afece76e
nschimme <nschimme@14c008a5-6e10-0410-bf79-c100e0f1932b>
2009-05-11 15:37:48 +0000

    Added a simple building script for newbies to compile
    git-svn-id: svn://svn.code.sf.net/p/mmapper/code/trunk/mmapper@149 14c008a5-6e10-0410-bf79-c100e0f1932b

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PROCESSORS=`grep processor /proc/cpuinfo | wc -l`
+echo Found $PROCESSORS processors
+declare -i JFLAG
+JFLAG=$PROCESSORS+1
+echo Going to run with $JFLAG jobs
+echo If you have problems make sure you have all the required dependencies
+echo \(i.e. libqt4-dev, libqt4-opengl-dev\)
+echo 
+cd src && make clean && qmake && make -j$JFLAG
