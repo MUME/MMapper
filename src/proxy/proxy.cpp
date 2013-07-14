4eb3361df3faf82c6e52c6f91e7f4f2db1b26985
klember <klember@14c008a5-6e10-0410-bf79-c100e0f1932b>
2013-07-14 19:48:27 +0000

    Turn on the XML parser after having requested the XML mode
    git-svn-id: svn://svn.code.sf.net/p/mmapper/code/trunk/mmapper@215 14c008a5-6e10-0410-bf79-c100e0f1932b

--- src/proxy/proxy.cpp
+++ src/proxy/proxy.cpp
@@ -217,6 +217,7 @@ bool Proxy::init()
     if (Config().m_mpi) {
       emit sendToMud(QByteArray("~$#EX1\n3\n"));
       emit log("Proxy", "Sent MUME Protocol Initiator XML request");
+      m_filter->setXmlMode();
     }
 
     return TRUE;
