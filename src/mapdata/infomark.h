0b802504a19deb6f246b67c753005a264d7d250e
Jean-Yves Perrier (teoli2003) <jypenator@gmail.com>
2016-12-14 13:36:27 +0000

    Adds internal support for InfoMark classes

--- src/mapdata/infomark.h
+++ src/mapdata/infomark.h
@@ -3,7 +3,7 @@
 ** Authors:   Ulf Hermann <ulfonk_mennhar@gmx.de> (Alve),
 **            Marek Krejza <krejza@gmail.com> (Caligor)
 **
-** This file is part of the MMapper project. 
+** This file is part of the MMapper project.
 ** Maintained by Nils Schimmelmann <nschimme@gmail.com>
 **
 ** This program is free software; you can redistribute it and/or
@@ -32,18 +32,20 @@
 typedef class QString InfoMarkName;
 typedef class QString InfoMarkText;
 enum InfoMarkType { MT_TEXT, MT_LINE, MT_ARROW };
+enum InfoMarkClass { MC_GENERIC, MC_HERB, MC_RIVER, MC_PLACE, MC_MOB, MC_COMMENT, MC_ROAD, MC_OBJECT };
 
 typedef QDateTime MarkerTimeStamp;
 
 class InfoMark {
 
 public:
-    InfoMark(){ m_type = MT_TEXT; }
+    InfoMark(){ m_type = MT_TEXT; m_class = MC_GENERIC; }
     ~InfoMark(){};
 
     const InfoMarkName& getName() const { return m_name; }
     const InfoMarkText& getText() const { return m_text; }
     InfoMarkType getType() const { return m_type; }
+    InfoMarkClass getClass() const {return m_class; }
     const Coordinate& getPosition1() const {return m_pos1;}
     const Coordinate& getPosition2() const {return m_pos2;}
     const MarkerTimeStamp& getTimeStamp() const { return m_timeStamp; }
@@ -52,15 +54,17 @@ public:
     void setPosition2(Coordinate & pos) {m_pos2 = pos;}
     void setName(InfoMarkName name) { m_name = name; }
     void setText(InfoMarkText text) { m_text = text; }
-    void setType(InfoMarkType type ){ m_type = type; }
-    
+    void setType(InfoMarkType type) { m_type = type; }
+    void setClass(InfoMarkClass markClass) { m_class = markClass; }
+
     void setTimeStamp(MarkerTimeStamp timeStamp) { m_timeStamp = timeStamp; }
 
 private:
     InfoMarkName m_name;
     InfoMarkType m_type;
     InfoMarkText m_text;
-    
+    InfoMarkClass m_class;
+
     MarkerTimeStamp m_timeStamp;
 
     Coordinate m_pos1;
