244ad281be3529bb95639a8e1564a1a4f25ea662
Nils Schimmelmann <nschimme@gmail.com>
2018-06-11 12:25:22 -0700

    Add option for include-what-you-use and clang-tidy static analyzer

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p build && cd build || exit 1
 
-cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. \
+cmake ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=. \
 	&& make -j$(getconf _NPROCESSORS_ONLN) \
 	&& make test \
 	&& make install
