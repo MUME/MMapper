e442f37a1b270d6742480dd72acc2ec00a4aceb0
Nils Schimmelmann <nschimme@gmail.com>
2018-09-27 19:32:00 -0700

    Update Latin-1 to US-ASCII translation to match MUME

--- src/parser/parserutils.cpp
+++ src/parser/parserutils.cpp
@@ -31,12 +31,15 @@
 
 namespace ParserUtils {
 
-// latin1 to 7-bit Ascii
+// Taken from MUME's HELP LATIN to convert from Latin-1 to US-ASCII
 static const unsigned char latin1ToAscii[] = {
-    /*192*/ 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I',
-    'D',         'N', 'O', 'O', 'O', 'O', 'O', 'x', 'O', 'U', 'U', 'U', 'U', 'Y', 'b', 'B',
-    'a',         'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i',
-    'o',         'n', 'o', 'o', 'o', 'o', 'o', ':', 'o', 'u', 'u', 'u', 'u', 'y', 'b', 'y'};
+    /*160*/
+    ' ', '!', 'c', 'L', '$',  'Y', '|', 'P', '"', 'C', 'a', '<', ',', '-', 'R', '-',
+    'd', '+', '2', '3', '\'', 'u', 'P', '*', ',', '1', 'o', '>', '4', '2', '3', '?',
+    'A', 'A', 'A', 'A', 'A',  'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I',
+    'D', 'N', 'O', 'O', 'O',  'O', 'O', '*', 'O', 'U', 'U', 'U', 'U', 'Y', 'T', 's',
+    'a', 'a', 'a', 'a', 'a',  'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i',
+    'd', 'n', 'o', 'o', 'o',  'o', 'o', '/', 'o', 'u', 'u', 'u', 'u', 'y', 't', 'y'};
 
 QString &removeAnsiMarksInPlace(QString &str)
 {
@@ -48,14 +51,13 @@ QString &removeAnsiMarksInPlace(QString &str)
 QString &latinToAsciiInPlace(QString &str)
 {
     unsigned char ch;
-    int pos;
-    for (pos = 0; pos < str.length(); pos++) {
+    for (int pos = 0; pos < str.length(); pos++) {
         ch = str.at(pos).toLatin1();
         if (ch > 128) {
-            if (ch < 192) {
+            if (ch < 160) {
                 ch = 'z';
             } else {
-                ch = latin1ToAscii[ch - 192];
+                ch = latin1ToAscii[ch - 160];
             }
             str[pos] = ch;
         }
