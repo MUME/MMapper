6d8ac855a5fbd8abfbc1c6dad8073b1fdd0d636c
Nils Schimmelmann <nschimme@gmail.com>
2019-09-16 22:54:12 -0500

    Delete build.sh and instead rely upon Github Wiki instructions

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p build && cd build || exit 1
-export QT_QPA_PLATFORM=offscreen
-cmake ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=. $1 \
-	&& make -j$(getconf _NPROCESSORS_ONLN) \
-	&& make test \
-	&& make install \
-	&& cpack
