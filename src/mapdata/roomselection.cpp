f540a7fff00e9cea49651f411eae58a7482222a6
Nils Schimmelmann <nschimme@gmail.com>
2019-10-12 19:35:09 -0700

    Unselecting temporary rooms should not mark them as permanent

--- src/mapdata/roomselection.cpp
+++ src/mapdata/roomselection.cpp
@@ -65,7 +65,7 @@ const Room *RoomSelection::getRoom(const Coordinate &coord)
 
 void RoomSelection::unselect(const RoomId targetId)
 {
-    m_mapData.keepRoom(*this, targetId);
+    m_mapData.releaseRoom(*this, targetId);
     remove(targetId);
 }
 
