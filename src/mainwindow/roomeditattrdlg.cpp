5140a372380eb4119afe5a4c1dd70b0c3cb1e5be
Elval Thorondil <ethorondil@gmail.com>
2020-11-07 23:40:17 -0600

    Allow changing online update status when multiple rooms are selected.

--- src/mainwindow/roomeditattrdlg.cpp
+++ src/mainwindow/roomeditattrdlg.cpp
@@ -577,8 +577,8 @@ void RoomEditAttrDlg::updateDialog(const Room *r)
         roomDescriptionTextEdit->clear();
         roomDescriptionTextEdit->setEnabled(false);
 
-        updatedCheckBox->setCheckable(false);
-        updatedCheckBox->setText("");
+        updatedCheckBox->setCheckable(true);
+        updatedCheckBox->setText("Online update status has not been changed.");
 
         roomNoteTextEdit->clear();
         roomNoteTextEdit->setEnabled(false);
