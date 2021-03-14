b6fb7e74226149655c4551180ed107be46a9df1a
Nils Schimmelmann <nschimme@gmail.com>
2021-03-13 19:29:44 -0600

    Fix invalid utf8 string processing

--- src/proxy/GmcpUtils.cpp
+++ src/proxy/GmcpUtils.cpp
@@ -13,27 +13,25 @@ namespace GmcpUtils {
 
 QString escapeGmcpStringData(const QString &str)
 {
-    std::ostringstream oss;
-    for (char c : str.toUtf8()) {
-        if (c == static_cast<char>(0xFF)) // 0xFF is not allowed as a valid character
-            continue;
-        else if (c == '"') // double-quote
-            oss << R"(\")";
-        else if (c == '\\') // backslash
-            oss << R"(\\)";
-        else if (c == C_NEWLINE) // new line
-            oss << R"(\n)";
-        else if (c == C_CARRIAGE_RETURN) // carriage-return
-            oss << R"(\r)";
-        else if (c == '\b') // backspace
-            oss << R"(\b)";
-        else if (c == '\f') // form-feed
-            oss << R"(\f)";
-        else if (c == C_TAB) // tab
-            oss << R"(\t)";
+    QString result;
+    for (const QChar c : str) {
+        if (c == '\"')
+            result.append(R"(\")");
+        else if (c == '\\')
+            result.append(R"(\\)");
+        else if (c == C_NEWLINE)
+            result.append(R"(\n)");
+        else if (c == C_CARRIAGE_RETURN)
+            result.append(R"(\r)");
+        else if (c == '\b')
+            result.append(R"(\b)");
+        else if (c == '\f')
+            result.append(R"(\f)");
+        else if (c == C_TAB)
+            result.append(R"(\t)");
         else
-            oss << c;
+            result.append(c);
     }
-    return ::toQStringUtf8(oss.str());
+    return result;
 }
 } // namespace GmcpUtils
