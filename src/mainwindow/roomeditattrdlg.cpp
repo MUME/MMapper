MUME/MMapper

commit ecfb2d9a9b4692985c8a03894f5ea5f0421a5f0a

bump minimum Qt to 5.15.0

src/mainwindow/roomeditattrdlg.cpp
@@ -478,12 +478,11 @@ const Room *RoomEditAttrDlg::getSelectedRoom()
     if (m_roomSelection->size() == 1) {
         return m_roomSelection->getFirstRoom();
     }
-    if (auto it = m_roomSelection->find(
-            RoomId{roomListComboBox->itemData(roomListComboBox->currentIndex()).toUInt()});
-        it != m_roomSelection->end()) {
-        return it->second;
-    }
-    return nullptr;
+    auto it = m_roomSelection->find(
+        RoomId{roomListComboBox->itemData(roomListComboBox->currentIndex()).toUInt()});
+    if (it != m_roomSelection->end())
+        return nullptr;
+    return it->second;
 }
 
 ExitDirEnum RoomEditAttrDlg::getSelectedExit()

CMakeLists.txt
@@ -38,8 +38,8 @@ set(CMAKE_AUTOUIC ON)
 set(CMAKE_AUTORCC ON)
 
 if(Qt5Core_FOUND)
-    if(Qt5Core_VERSION VERSION_LESS 5.12.4)
-        message(FATAL_ERROR "Minimum supported Qt version is 5.12")
+    if(Qt5Core_VERSION VERSION_LESS 5.15.0)
+        message(FATAL_ERROR "Minimum supported Qt version is 5.15")
     endif()
 endif()
 
@@ -183,8 +183,8 @@ endif()
 if(MMAPPER_IS_DEBUG)
     set(CMAKE_VERBOSE_MAKEFILE true)
 
-    # disable all the deprecated APIs in Qt <= 5.11
-    add_compile_options(-DQT_DISABLE_DEPRECATED_BEFORE=0x050b00)
+    # disable all the deprecated APIs in Qt <= 5.15
+    add_compile_options(-DQT_DISABLE_DEPRECATED_BEFORE=0x050F00)
 
     if(NOT MSVC)
         # common options for gcc and clang

src/CMakeLists.txt
@@ -685,13 +685,6 @@ if(WIN32)
     )
 
     # Bundle Library Files
-    if (Qt5Core_VERSION VERSION_LESS 5.14.0)
-       if(CMAKE_BUILD_TYPE_UPPER STREQUAL "DEBUG")
-          set(WINDEPLOYQT_ARGS --debug)
-       else()
-          set(WINDEPLOYQT_ARGS --release)
-        endif()
-    endif()
     set(WINDEPLOYQT_ARGS ${WINDEPLOYQT_ARGS} --dir ${CMAKE_CURRENT_BINARY_DIR}/bin --compiler-runtime --no-angle)
     find_program(WINDEPLOYQT_APP windeployqt HINTS ${QTDIR} ENV QTDIR PATH_SUFFIXES bin)
     message(" - windeployqt path: ${WINDEPLOYQT_APP}")

src/client/ClientTelnet.cpp
@@ -25,7 +25,7 @@ ClientTelnet::ClientTelnet(QObject *const parent)
     connect(&socket, &QAbstractSocket::disconnected, this, &ClientTelnet::slot_onDisconnected);
     connect(&socket, &QIODevice::readyRead, this, &ClientTelnet::slot_onReadyRead);
     connect(&socket,
-            QOverload<QAbstractSocket::SocketError>::of(&QTcpSocket::error),
+            QOverload<QAbstractSocket::SocketError>::of(&QAbstractSocket::errorOccurred),
             this,
             &ClientTelnet::slot_onError);
 }

src/client/inputwidget.cpp
@@ -222,7 +222,7 @@ void InputHistory::addInputLine(const QString &string)
 
 void TabHistory::addInputLine(const QString &string)
 {
-    QStringList list = string.split(s_whitespaceRx, QString::SkipEmptyParts);
+    QStringList list = string.split(s_whitespaceRx, Qt::SkipEmptyParts);
     for (const QString &word : list) {
         if (word.length() > MIN_WORD_LENGTH) {
             // Adding this word to the dictionary

src/display/MapCanvasData.cpp
@@ -92,8 +92,8 @@ glm::vec2 MapCanvasViewport::getMouseCoords(const QInputEvent *const event) cons
         const auto y = static_cast<float>(height() - mouse->pos().y());
         return glm::vec2{x, y};
     } else if (const auto *const wheel = dynamic_cast<const QWheelEvent *>(event)) {
-        const auto x = static_cast<float>(wheel->pos().x());
-        const auto y = static_cast<float>(height() - wheel->pos().y());
+        const auto x = static_cast<float>(wheel->position().x());
+        const auto y = static_cast<float>(height() - wheel->position().y());
         return glm::vec2{x, y};
     } else {
         throw std::invalid_argument("event");

src/main.cpp
@@ -80,12 +80,10 @@ static void useHighDpi()
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 }
 
-static void trySetHighDpiScaleFactorRoundingPolicy()
+static void setHighDpiScaleFactorRoundingPolicy()
 {
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
     QApplication::setHighDpiScaleFactorRoundingPolicy(
         Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-#endif
 }
 
 static void tryInitDrMingw()
@@ -168,7 +166,7 @@ static void setSurfaceFormat()
 int main(int argc, char **argv)
 {
     useHighDpi();
-    trySetHighDpiScaleFactorRoundingPolicy();
+    setHighDpiScaleFactorRoundingPolicy();
     setEnteredMain();
     if constexpr (IS_DEBUG_BUILD) {
         // see http://doc.qt.io/qt-5/qtglobal.html#qSetMessagePattern

src/mapfrontend/mapfrontend.h
@@ -47,7 +47,7 @@ protected:
     RoomLocks locks;
 
     RoomId greatestUsedId = INVALID_ROOMID;
-    QMutex mapLock{QMutex::Recursive};
+    QRecursiveMutex mapLock;
 
     struct Bounds final
     {

commit 763c80184ccd5131fe02baaa26bca7506dfaf6d8

fix crash when editing multiple rooms
Regression introduced with ecfb2d9a9b4692985c8a03894f5ea5f0421a5f0a

src/mainwindow/roomeditattrdlg.cpp
@@ -480,7 +480,7 @@ const Room *RoomEditAttrDlg::getSelectedRoom()
     }
     auto it = m_roomSelection->find(
         RoomId{roomListComboBox->itemData(roomListComboBox->currentIndex()).toUInt()});
-    if (it != m_roomSelection->end())
+    if (it == m_roomSelection->end())
         return nullptr;
     return it->second;
 }