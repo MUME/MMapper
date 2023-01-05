f283965c56116727a14133905d40f5b1df311600
Mike Repass <mike.repass@gmail.com>
2023-01-04 20:10:12 -0800

    CommsWindow starting to work

--- src/adventure/commswidget.cpp
+++ src/adventure/commswidget.cpp
@@ -1,4 +1,3 @@
-
 #include <QtCore>
 #include <QtWidgets>
 
@@ -13,8 +12,10 @@ CommsWidget::CommsWidget(AdventureJournal &aj, QWidget *parent)
     layout->setContentsMargins(0, 0, 0, 0);
     layout->setSpacing(0);
 
+    m_commsTextDocument = new QTextDocument(CommsWidget::DEFAULT_CONTENT);
+    m_commsCursor = new QTextCursor(m_commsTextDocument);
     m_commsTextEdit = new QTextEdit(this);
-    m_commsTextEdit->setPlainText("Narrates and Tells will appear here...");
+    m_commsTextEdit->setDocument(m_commsTextDocument);
 
     layout->addWidget(m_commsTextEdit);
 
@@ -27,6 +28,26 @@ CommsWidget::CommsWidget(AdventureJournal &aj, QWidget *parent)
 
 void CommsWidget::slot_onCommReceived(const QString &data)
 {
-    //qDebug() << "CommsWidget::slot_onCommReceived" << data;
-    m_commsTextEdit->append(data);
+    // If first message, clear the placeholder text
+    auto prepend = "\n";
+    if (m_numMessagesReceived == 0) {
+        m_commsTextDocument->clear();
+        prepend = "";
+    }
+    m_numMessagesReceived++;
+
+    m_commsCursor->movePosition(QTextCursor::End);
+    m_commsCursor->insertText(prepend + data);
+
+    // If more than MAX_LINES, preserve by deleting from the start
+    auto lines_over = m_commsTextDocument->lineCount() - CommsWidget::MAX_LINES;
+    if (lines_over > 0) {
+        m_commsCursor->movePosition(QTextCursor::Start);
+        m_commsCursor->movePosition(QTextCursor::Down, QTextCursor::KeepAnchor, lines_over);
+        m_commsCursor->removeSelectedText();
+        m_commsCursor->movePosition(QTextCursor::End);
+    }
+
+    auto scrollBar = m_commsTextEdit->verticalScrollBar();
+    scrollBar->setValue(scrollBar->maximum());
 }
--- src/adventure/commswidget.h
+++ src/adventure/commswidget.h
@@ -7,7 +7,11 @@
 class CommsWidget : public QWidget
 {
     Q_OBJECT
+
 public:
+    static const constexpr int MAX_LINES = 512;
+    static const constexpr auto DEFAULT_CONTENT = "Narrates and Tells will appear here...";
+
     explicit CommsWidget(AdventureJournal &aj, QWidget *parent = nullptr);
 
 public slots:
@@ -15,5 +19,10 @@ public slots:
 
 private:
     AdventureJournal &m_adventureJournal;
+
+    QTextDocument *m_commsTextDocument = nullptr;
+    QTextCursor *m_commsCursor = nullptr;
     QTextEdit *m_commsTextEdit = nullptr;
+
+    int m_numMessagesReceived = 0;
 };
