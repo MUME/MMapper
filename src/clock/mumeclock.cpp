dc9122315eee98d0bf78279132aeca69241b6e8e
mvancleave99 <53666391+mvancleave99@users.noreply.github.com>
2023-02-12 11:07:38 -0800

    Added warrens dawn/dusk messages
    Third commit of this change to fix error caused by extra white space at line 87.

--- src/clock/mumeclock.cpp
+++ src/clock/mumeclock.cpp
@@ -84,7 +84,7 @@ const QHash<QString, MumeTimeEnum> MumeClock::m_stringTimeHash{
     {"The sun slowly climbs from the vale of Anduin in the east.", MumeTimeEnum::DAWN},
     {"Shadows surround you as the sun disappears behind the peaks of the Hithaeglir.",
      MumeTimeEnum::DUSK},
-    
+
     // Outside Warrens
     {"Light gradually filters in, proclaiming a new sunrise.", MumeTimeEnum::DAWN},
     {"The deepening gloom announces another sunset.", MumeTimeEnum::DUSK},
