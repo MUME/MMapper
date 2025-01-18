0fbf68a965b8ee64d81b38818c53945585ca8b33
Nils Schimmelmann <nschimme@gmail.com>
2025-01-17 20:16:24 -0600

    add MMAPPER_WINDOW_TITLE_PROGRAM_FIRST environment variable

--- src/global/window_utils.cpp
+++ src/global/window_utils.cpp
@@ -20,8 +20,24 @@ void mmqt::rdisconnect(QObject *const obj)
 
 void mmqt::setWindowTitle2(QWidget &widget, const QString &program, const QString &title)
 {
-    // "untitled - MMapper"
-    // "Write your message to Gandalf. - MMapper Editor"
-    // "View text... - MMapper Viewer"
-    widget.setWindowTitle(QString("%1 - %2").arg(title, program));
+    // Maybe this should be a global config option?
+    static const auto programFirst = utils::getEnvBool("MMAPPER_WINDOW_TITLE_PROGRAM_FIRST")
+                                         .value_or(false);
+
+    // Many programs show "filename - program", and that works well in some cases,
+    // but in MMapper's case, the user does not have any control over the title
+    // of a remote edit / view, so the message shown in the taskbar can be confusing.
+    if (programFirst) {
+        // Choosing program first gives "program - filename", which at least lets the user
+        // see that it's a mmapper window:
+        // "MMapper - untitled"
+        // "MMapper Editor - Write your message to Gandalf."
+        // "MMapper Viewer - View text..."
+        widget.setWindowTitle(QString("%1 - %2").arg(program, title));
+    } else {
+        // "untitled - MMapper"
+        // "Write your message to Gandalf. - MMapper Editor"
+        // "View text... - MMapper Viewer"
+        widget.setWindowTitle(QString("%1 - %2").arg(title, program));
+    }
 }
