4be90ed906bd567770a0c72cfef0c11b19831288
Nils Schimmelmann <nschimme@gmail.com>
2025-06-26 10:01:50 -0500

    add boost license for immer to about dialog

--- src/mainwindow/aboutdialog.cpp
+++ src/mainwindow/aboutdialog.cpp
@@ -4,7 +4,8 @@
 
 #include "aboutdialog.h"
 
-#include "../global/TextUtils.h"
+#include "../global/ConfigConsts-Computed.h"
+#include "../global/ConfigEnums.h"
 #include "../global/Version.h"
 
 #include <QString>
@@ -12,7 +13,15 @@
 #include <QtGui>
 #include <QtWidgets>
 
-NODISCARD static QString getBuildInformation()
+namespace {
+struct LicenseInfo
+{
+    QString title;
+    QString introText;
+    QString resourcePath;
+};
+
+NODISCARD QString getBuildInformation()
 {
     const auto get_compiler = []() -> QString {
 #ifdef __clang__
@@ -30,11 +39,12 @@ NODISCARD static QString getBuildInformation()
     };
 
     return QString("Built on branch %1 using %2<br>")
-        .arg(QString::fromUtf8(getMMapperBranch()))
-        .arg(get_compiler());
+        .arg(QString::fromUtf8(getMMapperBranch()), get_compiler());
 }
 
-AboutDialog::AboutDialog(QWidget *parent)
+} // namespace
+
+AboutDialog::AboutDialog(QWidget *const parent)
     : QDialog(parent)
 {
     setWindowIcon(QIcon(":/icons/m.png"));
@@ -47,14 +57,14 @@ AboutDialog::AboutDialog(QWidget *parent)
         return "<p align=\"center\">"
                "<h3>"
                "<u>"
-               + tr("MMapper %1").arg(QString::fromUtf8(getMMapperVersion()))
+               + QString("MMapper %1").arg(QString::fromUtf8(getMMapperVersion()))
                + "</h3>"
                  "</u>"
                  "</p>"
                  "<p align=\"center\">"
                + getBuildInformation()
-               + tr("Based on Qt %1 (%2 bit)")
-                     .arg(QT_VERSION_STR)
+               + QString("Based on Qt %1 (%2 bit)")
+                     .arg(qVersion())
                      .arg(static_cast<size_t>(QSysInfo::WordSize))
                + "</p>";
     };
@@ -71,8 +81,7 @@ AboutDialog::AboutDialog(QWidget *parent)
         "Arfang, Cosmos, Cuantar, Elval, Kalev, Korir, Kovis, Krush, Mirnir, Taryn, Teoli, and Waba"
         "</p>"));
 
-    /* Licenses tab */
-    const auto loadLicenseResource = [](const QString &path) {
+    const auto loadResource = [](const QString &path) {
         QFile f(path);
         if (!f.open(QIODevice::ReadOnly | QIODevice::Text)) {
             return QString("Unable to open resource '%1'.").arg(path);
@@ -81,56 +90,97 @@ AboutDialog::AboutDialog(QWidget *parent)
             return ts.readAll();
         }
     };
-    licenseView->setText(
-        "<p>"
-        "This program is free software; you can redistribute it and/or "
-        "modify it under the terms of the GNU General Public License "
-        "as published by the Free Software Foundation; either version 2 "
-        "of the License, or (at your option) any later version."
-        "</p><p>"
-        "This program is distributed in the hope that it will be useful, "
-        "but WITHOUT ANY WARRANTY; without even the implied warranty of "
-        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
-        "</p><p>"
-        "See the GNU General Public License for more details. "
-        "</p>"
-        "<hr/><h1>GNU General Public License 2.0</h1>"
-        "<pre>"
-        + loadLicenseResource(":/LICENSE.GPL2")
-        + "</pre>"
-          "<hr/><h1>GNU Lesser General Public License 2.1</h1>"
-          "<p>Some versions of this product contains code from the "
-          "following LGPLed libraries: "
-          "<a href=\"https://github.com/jrfonseca/drmingw\">DrMingW</a></p>"
-          "<pre>"
-        + loadLicenseResource(":/LICENSE.LGPL")
-        + "</pre>"
-          "<hr/><h1>DejaVu Fonts License</h1>"
-          "<p>This license applies to the file <code>src/resources/fonts/DejaVuSansMono.ttf</code></p>"
-          "<pre>"
-        + loadLicenseResource(":/fonts/LICENSE")
-        + "</pre>"
-          "<hr/><h1>GLM License</h1>"
-          "<p>This product contains code from the "
-          "<a href=\"https://glm.g-truc.net/\">OpenGL Mathematics (GLM)</a>"
-          " project.</p>"
-          "<pre>"
-        + loadLicenseResource(":/LICENSE.GLM")
-        + "</pre>"
-          "<hr/><h1>QtKeychain License</h1>"
-          "<p>This product contains code from the "
-          "<a href=\"https://github.com/frankosterfeld/qtkeychain\">QtKeychain</a>"
-          " project.</p>"
-          "<pre>"
-        + loadLicenseResource(":/LICENSE.QTKEYCHAIN")
-        + "</pre>"
-          "<hr/><h1>OpenSSL License</h1>"
-          "<p>Some versions of this product contains code from the "
-          "<a href=\"https://www.openssl.org/\">OpenSSL toolkit</a>"
-          ".</p>"
-          "<pre>"
-        + loadLicenseResource(":/LICENSE.OPENSSL") + "</pre>");
-    setFixedFont(licenseView);
+
+    /* Licenses tab */
+    QList<LicenseInfo> licenses;
+    licenses.append({"GNU General Public License 2.0",
+                     "<p>"
+                     "This program is free software; you can redistribute it and/or "
+                     "modify it under the terms of the GNU General Public License "
+                     "as published by the Free Software Foundation; either version 2 "
+                     "of the License, or (at your option) any later version."
+                     "</p>"
+                     "<p>"
+                     "This program is distributed in the hope that it will be useful, "
+                     "but WITHOUT ANY WARRANTY; without even the implied warranty of "
+                     "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."
+                     "</p>"
+                     "<p>"
+                     "See the GNU General Public License for more details. "
+                     "</p>",
+                     ":/LICENSE.GPL2"});
+
+    licenses.append({"DejaVu Fonts License",
+                     "<p>"
+                     "This license applies to the file "
+                     "<code>src/resources/fonts/DejaVuSansMono.ttf</code>"
+                     "</p>",
+                     ":/fonts/LICENSE"});
+
+    licenses.append({"GLM License",
+                     "<p>"
+                     "This product contains code from the "
+                     "<a href=\"https://glm.g-truc.net/\">OpenGL Mathematics (GLM)</a>"
+                     " project."
+                     "</p>",
+                     ":/LICENSE.GLM"});
+
+    licenses.append({"QtKeychain License",
+                     "<p>"
+                     "This product contains code from the "
+                     "<a href=\"https://github.com/frankosterfeld/qtkeychain\">QtKeychain</a>"
+                     " project."
+                     "</p>",
+                     ":/LICENSE.QTKEYCHAIN"});
+
+    licenses.append({"OpenSSL License",
+                     "<p>"
+                     "Some versions of this product contains code from the "
+                     "<a href=\"https://www.openssl.org/\">OpenSSL toolkit</a>."
+                     "</p>",
+                     ":/LICENSE.OPENSSL"});
+
+    licenses.append({"Boost Software License 1.0",
+                     "<p>"
+                     "This product contains code from the "
+                     "<a href=\"https://github.com/arximboldi/immer\">immer</a>"
+                     " project."
+                     "</p>",
+                     ":/LICENSE.BOOST"});
+
+    if constexpr (CURRENT_PLATFORM == PlatformEnum::Windows) {
+        licenses.append({"GNU Lesser General Public License 2.1",
+                         "<p>"
+                         "Some versions of this product contains code from the "
+                         "following LGPLed libraries: "
+                         "<a href=\"https://github.com/jrfonseca/drmingw\">DrMingW</a>"
+                         "</p>",
+                         ":/LICENSE.LGPL"});
+    }
+
+    for (const auto &license : licenses) {
+        auto *titleLabel = new QLabel(QStringLiteral("<h2>%1</h2>").arg(license.title));
+        titleLabel->setTextFormat(Qt::RichText);
+        licenseLayout->addWidget(titleLabel);
+
+        if (!license.introText.isEmpty()) {
+            auto *introLabel = new QLabel(license.introText);
+            introLabel->setWordWrap(true);
+            introLabel->setTextFormat(Qt::RichText);
+            licenseLayout->addWidget(introLabel);
+        }
+
+        auto *textEdit = new QTextEdit;
+        textEdit->setReadOnly(true);
+        textEdit->setPlainText(loadResource(license.resourcePath));
+        textEdit->setFixedHeight(160);
+        licenseLayout->addWidget(textEdit);
+
+        auto *hr = new QFrame;
+        hr->setFrameShape(QFrame::HLine);
+        hr->setFrameShadow(QFrame::Sunken);
+        licenseLayout->addWidget(hr);
+    }
 
     adjustSize();
 }
