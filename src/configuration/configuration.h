1b91f2090f56b4da63debdfb95e8cc73a21a28c7
Lewis Buckingham <sunnylew75@hotmail.com>
2025-07-19 16:51:50 +1000

    config: add showNpcGhosts flag (default true)

--- src/configuration/configuration.h
+++ src/configuration/configuration.h
@@ -291,6 +291,7 @@ public:
         bool showTokens   = true;
         bool showMapTokens = true;
         int tokenIconSize = 32;
+        bool showNpcGhosts  = true;
         QMap<QString, QString> tokenOverrides;
 
     private:
