1bbcb9e5bf62c16dd86faafc1fb68d14cc074a08
Lewis Buckingham <sunnylew75@hotmail.com>
2025-07-21 12:36:08 +1000

    feat: preferences checkbox & build tweaks for NPC ghost feature

--- src/display/GhostRegistry.h
+++ src/display/GhostRegistry.h
@@ -1,14 +1,10 @@
 #pragma once
 #include <unordered_map>
 #include <QString>
-
-
-#include "../map/roomid.h"
-#include "../group/CGroupChar.h"
+#include "../map/roomid.h"         // ServerRoomId
 
 struct GhostInfo {
-    ServerRoomId roomSid;
-    QString      tokenKey;
+    QString tokenKey;             // icon to draw (display name)
 };
 
-extern std::unordered_map<GroupId, GhostInfo> g_ghosts;
+extern std::unordered_map<ServerRoomId, GhostInfo> g_ghosts;
