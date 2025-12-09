MUME/MMapper

commit 3edbb2f1c47d09f541b3e75143a2c7d366a3f633

refactor top level widgets to use a non-modal QDialog

src/mpi/remoteeditwidget.cpp
@@ -28,14 +28,12 @@
 
 #include <QAction>
 #include <QMenu>
-#include <QMenuBar>
 #include <QMessageBox>
 #include <QMessageLogContext>
 #include <QPlainTextEdit>
 #include <QRegularExpression>
 #include <QScopedPointer>
 #include <QSize>
-#include <QStatusBar>
 #include <QString>
 #include <QTextDocument>
 #include <QtGui>
@@ -683,23 +681,25 @@ RemoteEditWidget::RemoteEditWidget(const bool editSession,
                                    QString title,
                                    QString body,
                                    QWidget *const parent)
-    : QMainWindow(parent)
+    : QDialog(parent)
     , m_editSession(editSession)
     , m_title(std::move(title))
     , m_body(std::move(body))
 {
-    setWindowFlags(windowFlags() | Qt::WindowType::Widget);
     setWindowFlags(windowFlags() & ~Qt::WindowStaysOnTopHint);
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
-
     mmqt::setWindowTitle2(*this,
                           QString("MMapper %1").arg(m_editSession ? "Editor" : "Viewer"),
                           m_title);
 
-    QWidget *centralContainer = new QWidget(this);
-    QVBoxLayout *mainLayout = new QVBoxLayout(centralContainer);
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
     mainLayout->setContentsMargins(0, 0, 0, 0);
     mainLayout->setSpacing(0);
+
+    setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+
+    m_menuBar = new QMenuBar(this);
+    mainLayout->setMenuBar(m_menuBar);
 
     m_gotoWidget.reset(createGotoWidget());
     mainLayout->addWidget(m_gotoWidget.get(), 0);
@@ -707,15 +707,14 @@ RemoteEditWidget::RemoteEditWidget(const bool editSession,
     m_findReplaceWidget.reset(createFindReplaceWidget());
     mainLayout->addWidget(m_findReplaceWidget.get(), 0);
 
-    // REVISIT: can this be called as an initializer?
-    // Probably not. In fact this may be too early, since it accesses contentsMargins(),
-    // which assumes this object is fully constructed and initialized.
-    //
-    // REVISIT: does this have to be QScopedPointer, or can it be std::unique_ptr?
     m_textEdit.reset(createTextEdit());
     mainLayout->addWidget(m_textEdit.get(), 1);
 
-    setCentralWidget(centralContainer);
+    m_statusBar = new QStatusBar(this);
+    mainLayout->addWidget(m_statusBar);
+
+    addStatusBar(m_textEdit.get());
+    addFileMenu(m_textEdit.get());
 
     // REVISIT: Restore geometry from config?
     setGeometry(QStyle::alignedRect(Qt::LeftToRight,
@@ -749,9 +748,6 @@ auto RemoteEditWidget::createTextEdit() -> Editor *
 
     auto *const doc = pTextEdit->document();
     new LineHighlighter(MAX_LENGTH, doc);
-
-    addStatusBar(pTextEdit);
-    addFileMenu(menuBar(), pTextEdit);
     return pTextEdit;
 }
 
@@ -809,14 +805,14 @@ auto RemoteEditWidget::createFindReplaceWidget() -> FindReplaceWidget *
     return pFindReplaceWidget;
 }
 
-void RemoteEditWidget::addFileMenu(QMenuBar *const menuBar, const Editor *const pTextEdit)
+void RemoteEditWidget::addFileMenu(const Editor *const pTextEdit)
 {
-    QMenu *const fileMenu = menuBar->addMenu(tr("&File"));
+    QMenu *const fileMenu = m_menuBar->addMenu(tr("&File"));
     if (m_editSession) {
         addSave(fileMenu);
     }
     addExit(fileMenu);
-    addEditAndViewMenus(menuBar, pTextEdit);
+    addEditAndViewMenus(pTextEdit);
 }
 
 struct NODISCARD EditViewCommand final
@@ -870,10 +866,10 @@ struct NODISCARD EditCommand2 final
     {}
 };
 
-void RemoteEditWidget::addEditAndViewMenus(QMenuBar *const menuBar, const Editor *const pTextEdit)
+void RemoteEditWidget::addEditAndViewMenus(const Editor *const pTextEdit)
 {
-    QMenu *const editMenu = menuBar->addMenu("&Edit");
-    QMenu *const viewMenu = menuBar->addMenu("&View");
+    QMenu *const editMenu = m_menuBar->addMenu("&Edit");
+    QMenu *const viewMenu = m_menuBar->addMenu("&View");
 
     const std::vector<EditCommand2> cmds{
 
@@ -1069,8 +1065,7 @@ void RemoteEditWidget::addToMenu(QMenu *const menu,
 
 void RemoteEditWidget::addStatusBar(const Editor *const pTextEdit)
 {
-    QStatusBar *const status = statusBar();
-    status->showMessage(tr("Ready"));
+    m_statusBar->showMessage(tr("Ready"));
 
     connect(pTextEdit,
             &QPlainTextEdit::cursorPositionChanged,
@@ -1088,7 +1083,7 @@ void RemoteEditWidget::slot_updateStatus(const QString &message_param)
     if (message_param.isEmpty()) {
         slot_updateStatusBar();
     } else {
-        statusBar()->showMessage(message_param);
+        m_statusBar->showMessage(message_param);
     }
 }
 
@@ -1331,7 +1326,7 @@ void RemoteEditWidget::slot_updateStatusBar()
         }
     };
     report_errors();
-    statusBar()->showMessage(status);
+    m_statusBar->showMessage(status);
 }
 
 void RemoteEditWidget::slot_justifyText()
@@ -1501,6 +1496,13 @@ bool RemoteEditWidget::slot_maybeCancel()
     return true;
 }
 
+/* Qt virtual */
+void RemoteEditWidget::showEvent(QShowEvent *event)
+{
+    QDialog::showEvent(event);
+    slot_updateStatusBar();
+}
+
 bool RemoteEditWidget::slot_contentsChanged() const
 {
     const QString text = m_textEdit->toPlainText();
@@ -1519,12 +1521,4 @@ void RemoteEditWidget::slot_finishEdit()
     m_submitted = true;
     emit sig_save(m_textEdit->toPlainText());
     close();
-}
-
-void RemoteEditWidget::setVisible(bool visible)
-{
-    QWidget::setVisible(visible);
-    if (visible) {
-        slot_updateStatusBar();
-    }
 }

src/viewers/TopLevelWindows.cpp
@@ -12,7 +12,7 @@
 #include <memory>
 
 #include <QDebug>
-#include <QMainWindow>
+#include <QDialog>
 #include <QPointer>
 #include <QTimer>
 
@@ -28,12 +28,12 @@ private:
     class NODISCARD Entry final
     {
     private:
-        std::unique_ptr<QMainWindow> m_unique;
+        std::unique_ptr<QDialog> m_unique;
         QString m_name;
-        QPointer<QMainWindow> m_weak_ptr;
+        QPointer<QDialog> m_weak_ptr;
 
     public:
-        explicit Entry(std::unique_ptr<QMainWindow> ptr)
+        explicit Entry(std::unique_ptr<QDialog> ptr)
             : m_unique{std::move(ptr)}
             , m_name{deref(m_unique).windowTitle()}
             , m_weak_ptr{m_unique.get()}
@@ -51,14 +51,14 @@ private:
         NODISCARD const QString &getName() const { return m_name; }
 
     private:
-        NODISCARD QMainWindow *try_get() { return m_weak_ptr.data(); }
-        NODISCARD const QMainWindow *try_get() const { return m_weak_ptr.data(); }
+        NODISCARD QDialog *try_get() { return m_weak_ptr.data(); }
+        NODISCARD const QDialog *try_get() const { return m_weak_ptr.data(); }
 
     public:
         NODISCARD bool isVisible() const
         {
-            if (const QMainWindow *const ptr = try_get()) {
-                const QMainWindow &w = deref(ptr);
+            if (const QDialog *const ptr = try_get()) {
+                const QDialog &w = deref(ptr);
                 if (w.isVisible()) {
                     return true;
                 }
@@ -68,7 +68,7 @@ private:
 
         void disconnectAllChildren()
         {
-            if (QMainWindow *const ptr = try_get()) {
+            if (QDialog *const ptr = try_get()) {
                 if (verbose_debugging) {
                     qDebug() << "Disconnecting all chidren of window" << getName();
                 }
@@ -78,8 +78,8 @@ private:
 
         void deleteWindowLater()
         {
-            if (QMainWindow *const ptr = try_get()) {
-                QMainWindow &w = deref(ptr);
+            if (QDialog *const ptr = try_get()) {
+                QDialog &w = deref(ptr);
                 if (verbose_debugging) {
                     qDebug() << "Marking window" << getName() << "for destruction.";
                 }
@@ -160,7 +160,7 @@ private:
     }
 
 public:
-    void add(std::unique_ptr<QMainWindow> pWindow)
+    void add(std::unique_ptr<QDialog> pWindow)
     {
         auto &window = deref(pWindow);
         const auto title = window.windowTitle();
@@ -205,7 +205,7 @@ void destroyTopLevelWindows()
     g_topLevelWindows.reset();
 }
 
-void addTopLevelWindow(std::unique_ptr<QMainWindow> window)
+void addTopLevelWindow(std::unique_ptr<QDialog> window)
 {
     ABORT_IF_NOT_ON_MAIN_THREAD();
     deref(g_topLevelWindows).add(std::move(window));

src/mpi/remoteeditwidget.h
@@ -9,14 +9,12 @@
 #include <memory>
 
 #include <QAction>
-#include <QMainWindow>
+#include <QDialog>
 #include <QPlainTextEdit>
 #include <QScopedPointer>
 #include <QSize>
 #include <QString>
 #include <QtCore>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QVBoxLayout>
 
 struct EditViewCommand;
 struct EditCommand2;
@@ -27,8 +25,9 @@ class QMenu;
 class QMenuBar;
 class QObject;
 class QPlainTextEdit;
-class QWidget;
 class QStatusBar;
+class QVBoxLayout;
+class QWidget;
 class GotoWidget;
 class FindReplaceWidget;
 
@@ -132,9 +131,13 @@ private:
     void handle_toolTip(QEvent *event) const;
 };
 
-class NODISCARD_QOBJECT RemoteEditWidget : public QMainWindow
+class NODISCARD_QOBJECT RemoteEditWidget : public QDialog
 {
     Q_OBJECT
+
+private:
+    QMenuBar *m_menuBar;
+    QStatusBar *m_statusBar;
 
 public:
     using Editor = RemoteTextEdit;
@@ -159,8 +162,8 @@ public:
     NODISCARD QSize sizeHint() const override;
     void closeEvent(QCloseEvent *event) override;
 
-public:
-    void setVisible(bool visible) override;
+protected:
+    void showEvent(QShowEvent *event) override;
 
 private:
     NODISCARD Editor *createTextEdit();
@@ -170,8 +173,8 @@ private:
     void addToMenu(QMenu *menu, const EditViewCommand &cmd);
     void addToMenu(QMenu *menu, const EditCommand2 &cmd, const Editor *pTextEdit);
 
-    void addFileMenu(QMenuBar *menuBar, const Editor *pTextEdit);
-    void addEditAndViewMenus(QMenuBar *menuBar, const Editor *pTextEdit);
+    void addFileMenu(const Editor *pTextEdit);
+    void addEditAndViewMenus(const Editor *pTextEdit);
     void addSave(QMenu *fileMenu);
     void addExit(QMenu *fileMenu);
     void addStatusBar(const Editor *pTextEdit);

src/viewers/AnsiViewWindow.cpp
@@ -10,19 +10,23 @@
 
 #include <tuple>
 
-#include <QMainWindow>
+#include <QDialog>
 #include <QStyle>
 #include <QTextBrowser>
+#include <QVBoxLayout>
 
 AnsiViewWindow::AnsiViewWindow(const QString &program,
                                const QString &title,
                                const std::string_view message)
     : m_view{std::make_unique<QTextBrowser>(this)}
 {
-    setWindowFlags(windowFlags() | Qt::WindowType::Widget);
     setWindowFlags(windowFlags() & ~Qt::WindowStaysOnTopHint);
     setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
     mmqt::setWindowTitle2(*this, program, title);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    mainLayout->setSpacing(0);
 
     auto &view = deref(m_view);
     setAnsiText(&view, message);
@@ -38,7 +42,7 @@ AnsiViewWindow::AnsiViewWindow(const QString &program,
     show();
     raise();
     activateWindow();
-    setCentralWidget(&view);
+    mainLayout->addWidget(&view, 0);
     view.setFocus(); // REVISIT: can this be done in the creation function?
 }
 

src/viewers/AnsiViewWindow.h
@@ -7,12 +7,12 @@
 #include <memory>
 #include <string_view>
 
-#include <QMainWindow>
+#include <QDialog>
 #include <QString>
 
 class QTextBrowser;
 
-class NODISCARD AnsiViewWindow final : public QMainWindow
+class NODISCARD AnsiViewWindow final : public QDialog
 {
 private:
     std::unique_ptr<QTextBrowser> m_view;

src/viewers/TopLevelWindows.h
@@ -5,8 +5,8 @@
 #include <memory>
 
 class QString;
-class QMainWindow;
+class QDialog;
 
 extern void initTopLevelWindows();
 extern void destroyTopLevelWindows();
-extern void addTopLevelWindow(std::unique_ptr<QMainWindow> window);
+extern void addTopLevelWindow(std::unique_ptr<QDialog> window);

commit 3b9e8c0091226b79a635cbb5749a2e27221504be

add back missing window title buttons
This was broken in 3edbb2f1c47d09f541b3e75143a2c7d366a3f633

src/mpi/remoteeditwidget.cpp
@@ -686,8 +686,8 @@ RemoteEditWidget::RemoteEditWidget(const bool editSession,
     , m_title(std::move(title))
     , m_body(std::move(body))
 {
-    setWindowFlags(windowFlags() & ~Qt::WindowStaysOnTopHint);
-    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setWindowFlags(Qt::Window | Qt::WindowSystemMenuHint | Qt::WindowMinimizeButtonHint
+                   | Qt::WindowMaximizeButtonHint | Qt::WindowCloseButtonHint);
     mmqt::setWindowTitle2(*this,
                           QString("MMapper %1").arg(m_editSession ? "Editor" : "Viewer"),
                           m_title);

src/viewers/TopLevelWindows.cpp
@@ -164,6 +164,8 @@ public:
     {
         auto &window = deref(pWindow);
         const auto title = window.windowTitle();
+        window.setWindowFlags(Qt::Window | Qt::WindowSystemMenuHint | Qt::WindowMinimizeButtonHint
+                              | Qt::WindowMaximizeButtonHint | Qt::WindowCloseButtonHint);
 
         if (!window.isVisible()) {
             window.setVisible(true);